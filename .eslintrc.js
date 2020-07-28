6679791296a9534bdd05312b08c45b0f02f9486c
rx <1003142361@qq.com>
2020-07-29 00:12:49 +0800

    配置一些eslint

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+/*eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended"
+  ],
+  root:true,
+  parserOptions: {
+    parser:"babel-eslint"
+  },
+  plugins: [
+    'vue'
+  ],
+  rules: {
+    'vue/no-multiple-template-root': "off",
+    'vue/valid-template-root': "off",
+    "vue/html-self-closing": "off",
+    "vue/singleline-html-element-content-newline":"off",
+    "vue/require-default-prop":"off",
+    "vue/max-attributes-per-line": ["error", {
+      "singleline": 2,
+      "multiline": {
+        "max": 2,
+        "allowFirstLine": true
+      }
+    }]
+  }
+}
